e768849206ac02ea635b9ff4a6225bb89da95093
belcherman2 <rlenihan@openadmin.com>
2022-12-22 12:20:15 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+	<p>I was changed on the feature branch</p>
         </p>
         <a
           className="App-link"
